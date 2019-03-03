59ecc6ae2d99a8f5128984388f3c2cad5137ef31
UPPERCASEuser <pedro.santos.cartaxo@gmail.com>
2019-03-02 23:43:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
 	epochToDate: function (epoch) {
-		return new Date("2016-3-17").valueOf()
+		return new Date(epoch).valueOf()
 	}
 	dateToEpoch: function (date) {
 		return new Date(date).toLocaleString();
